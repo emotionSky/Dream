e831a14331387ca05e9628b0a6355aefa3b88560
emotionSky <bitdreamsky@126.com>
2023-09-03 20:45:20 +0800

    1. 新结构库

---
+++ DreamCommon/vcproj/postbuild.bat
@@ -0,0 +1,18 @@
+@REM 设置一些路径
+set project_name=DreamCommon
+set bin_base=.\bin
+set export_base=..\..\3d_part
+
+@REM 判断路径是否存在从而是否需要新建
+set dst_base=%export_base%\%project_name%
+if exist %dst_base% (
+	rmdir /s /q %dst_base%
+)
+
+mkdir %dst_base%\include\DreamSky
+mkdir %dst_base%\lib
+
+@REM 拷贝文件
+copy ..\include\DreamSky\*.h %dst_base%\include\DreamSky\
+copy %bin_base%\*.dll %dst_base%\lib\
+copy %bin_base%\*.lib %dst_base%\lib\
---
+++ DreamConfig/README.md
@@ -0,0 +1,2 @@
+# DreamConfig
+一些常用的配置文件读写库，比如ini/json等
